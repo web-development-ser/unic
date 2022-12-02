7596552d8ecfe90daac7c9d650e4aab4b67d7e34
sergiosantos0880@gmail.com <sergiosantos0880@gmail.com>
2022-12-02 07:58:46 -0300

    0001

---
+++ main.js
@@ -0,0 +1,14 @@
+function unic () {
+  const text = document.getElementById('text');
+  text.classList.add('text')
+  text.innerHTML = "Não seja funda para pessoas rasas..."
+  tex.innerHTML = "Nem todos os capítulos mau feito descrevem histórias fáceis, <span>ainda existem páginas em brancas...</span>"
+  
+  
+  
+  
+  
+  
+  
+  console.log("Hello")
+}
